7081488018511c834792428736b0409a9abfa0f9
kerolous samy <keroloussamy98@gmail.com>
2022-03-04 01:16:39 +0200

    Add display_list method

--- main.rb
+++ main.rb
@@ -1,3 +1,19 @@
+require './student'
+require './teacher'
+require './book'
+require './rental'
+
+def display_list
+  puts "
+    Please choose an option by entering a number:
+    1- List all books.
+    2- List all people.
+    3- Create a person (teacher or student, not a plain Person).
+    4- Create a book.
+    5- Create a rental.
+    6- List all rentals for a given person id.
+    7- Exit"
+end
 
 def switch_case(books, people, rentals)
   loop do
